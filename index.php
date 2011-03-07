c9d8fa008237d0437be04462a27b7a62d3b58d88
lrewega <lrewega@wert.ca>
2011-03-07 08:45:38 -0500

    Initial commit with a template directory with some files to start with

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>App Template</title>
+    <meta name="description" content="">
+    <meta name="author" content="The Guelph Seven">
+    <meta name="keywords" content="guelph seven guelphseven 7 cubed android startups development coding apps uog uoguelph uwaterloo">
+    <meta name="robots" content="index, follow">
+    <meta name="viewport" content="width=device-width">
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <link rel="stylesheet" href="/css/style.css">
+    <script src="/js/libs/modernizr-1.6.min.js"></script>
+</head>
+<body>
+    <div id="container">
+        <header>
+        </header>
+        <footer>
+        </footer>
+    </div>
+    <!--[if lt IE 7 ]>
+    <script src="/js/libs/dd_belatedpng.js"></script>
+    <script> DD_belatedPNG.fix('img, .ipng_bg'); </script>
+    <![endif]-->
+    <script src="http://static.getclicky.com/js"></script> 
+    <noscript><p><img alt="Clicky" width="1" height="1" src="http://in.getclicky.com/XXXXXXXXns.gif" /></p></noscript>
+    <script>
+        var _gaq = [['_setAccount', 'XX-XXXXXXXX-X'], ['_trackPageview']];
+        (function(d, t) {
+            var g = d.createElement(t),
+            s = d.getElementsByTagName(t)[0];
+            g.async = true;
+            g.src = ('https:' == location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g, s);
+        })(document, 'script');
+        try{ clicky.init(XXXXXXXX); }catch(err){}
+    </script>
+</body>
+</html>
